05bb4d13fd867146f23e8218f42a56afbbae4936
Sylvia452 <SidneyGardinerdyajf@outlook.com>
2025-11-11 10:52:00 +0800

    feat: add deployment scripts and test files

---
+++ deploy/deploy.ts
@@ -0,0 +1,18 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  const deployedSecretVoteBox = await deploy("SecretVoteBox", {
+    from: deployer,
+    log: true,
+  });
+
+  console.log(`SecretVoteBox contract deployed at: ${deployedSecretVoteBox.address}`);
+  console.log(`Deployment transaction hash: ${deployedSecretVoteBox.transactionHash}`);
+};
+export default func;
+func.id = "deploy_secretVoteBox"; // id required to prevent reexecution
+func.tags = ["SecretVoteBox"];
